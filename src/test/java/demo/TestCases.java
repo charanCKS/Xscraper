176621560a594d4750b1a0872a7c4270accb10c7
Charan K <charancks@gmail.com>
2024-06-29 15:29:07 +0530

    ME_QA_XSCRAPE_DATA_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,21 +1,34 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
     ChromeDriver driver;
+    private Wrappers wrappers;
+    
+
 
     /*
      * TODO: Write your tests here with testng @Test annotation. 
@@ -47,6 +60,8 @@ public class TestCases {
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+        wrappers = new Wrappers(driver);
+
     }
 
     @AfterTest
@@ -56,4 +71,121 @@ public class TestCases {
         driver.quit();
 
     }
+
+    @Test
+    public void testCase01() throws IOException {
+        System.out.println("Test case 01 started");
+        wrappers.navigateToUrl("https://www.scrapethissite.com/pages/");
+        wrappers.clickElement(By.linkText("Hockey Teams: Forms, Searching and Pagination"));
+
+        wrappers.waitForPageToLoad();
+        List<HashMap<String, Object>> hockeyData = new ArrayList<>();
+
+        for (int i = 1; i <= 4; i++) {
+            List<WebElement> rows = wrappers.findElements(By.xpath("//table[@class='table']//tr[@class='team']"));
+            
+            for (WebElement row : rows) {
+                List<WebElement> cols = row.findElements(By.tagName("td"));
+            
+                if (cols.size() > 0) {
+                    String teamName = cols.get(0).getText();
+                    String year = cols.get(1).getText();
+                    String winPercentageStr = cols.get(5).getText().trim();
+                    double winPercentage = Double.parseDouble(winPercentageStr);
+
+                    if (winPercentage < 0.40) {
+                        HashMap<String, Object> teamData = new HashMap<>();
+                        teamData.put("Epoch Time of Scrape", System.currentTimeMillis());
+                        teamData.put("Team Name", teamName);
+                        teamData.put("Year", year);
+                        teamData.put("Win %", winPercentageStr);
+                        hockeyData.add(teamData);
+                    }
+                }
+            }
+
+            // Check if there is a next page and click it
+            WebElement nextButton = driver.findElement(By.xpath("//a[@aria-label='Next']"));
+            if (nextButton != null && nextButton.isDisplayed() && nextButton.isEnabled()) {
+                nextButton.click();
+                wrappers.waitForPageToLoad();
+            } else {
+                System.out.println("Next Button is not clicked");
+                break;
+            }
+        }
+
+        // Convert ArrayList to JSON and save to file
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(new File("hockey-team-data.json"), hockeyData);
+        System.out.println("Test case 01 completed");
+    }
+
+    @Test
+    public void testCase02() throws IOException {
+        System.out.println("Test case 02 started");
+        wrappers.navigateToUrl("https://www.scrapethissite.com/pages/");
+        wrappers.clickElement(By.linkText("Oscar Winning Films: AJAX and Javascript"));
+
+        wrappers.waitForPageToLoad();
+        List<HashMap<String, Object>> oscarData = new ArrayList<>();
+
+        List<WebElement> yearLinks = driver.findElements(By.cssSelector(".year-link"));
+
+        for (WebElement yearLink : yearLinks) {
+            String year = yearLink.getText();
+            yearLink.click();
+            wrappers.waitForElement(By.xpath("//tbody[@id='table-body']//tr"));
+
+            List<WebElement> movieRows = driver.findElements(By.xpath("//tbody[@id='table-body']//tr"));
+            for (int i = 0; i < Math.min(5, movieRows.size()); i++) {
+                WebElement row = movieRows.get(i);
+                List<WebElement> cols = row.findElements(By.tagName("td"));
+
+                if (cols.size() > 0) {
+                    String title = cols.get(0).getText();
+                    String nomination = cols.get(1).getText();
+                    String awards = cols.get(2).getText();
+                    
+                    boolean isWinner = false;
+                    WebElement tdElement = cols.get(3); // Assuming cols.get(3) is the fourth column (index 3)
+                    List<WebElement> iconElements = tdElement.findElements(By.cssSelector("i.glyphicon"));
+                    for (WebElement iconElement : iconElements) {
+                    if (iconElement.getAttribute("class").contains("glyphicon")) {
+                    isWinner = true;
+                    break; // Exit the loop if glyphicon class is found
+                    }
+}
+
+
+                    HashMap<String, Object> movieData = new HashMap<>();
+                    movieData.put("Epoch Time of Scrape", System.currentTimeMillis());
+                    movieData.put("Year", year);
+                    movieData.put("Title", title);
+                    movieData.put("Nomination", nomination);
+                    movieData.put("Awards", awards);
+                    movieData.put("isWinner", isWinner);
+                    oscarData.add(movieData);                    
+                }
+            }
+
+            // Go back to the year selection page
+            driver.navigate().back();
+            wrappers.waitForPageToLoad();
+        }
+
+        // Convert ArrayList to JSON and save to file
+        File outputFile = new File("output/oscar-winner-data.json");
+        outputFile.getParentFile().mkdirs(); // Create directories if they do not exist
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputFile, oscarData);
+
+        // Assert that the file is created and is not empty
+        Assert.assertTrue(outputFile.exists(), "File does not exist");
+        Assert.assertTrue(outputFile.length() > 0, "File is empty");
+
+        System.out.println("Test case 02 completed");
+    }
+
+
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -3,9 +3,11 @@ package demo.wrappers;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,4 +18,44 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    private WebDriver driver;
+
+    public Wrappers(WebDriver driver) {
+        this.driver = driver;
+    }
+
+     public void clickElement(By locator) {
+        WebElement element = waitForElement(locator);
+        element.click();
+    }
+
+    public WebElement findElement(By locator) {
+        return driver.findElement(locator);
+    }
+
+    public List<WebElement> findElements(By locator) {
+        return driver.findElements(locator);
+    }
+
+    public WebElement waitForElement(By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    public void navigateToUrl(String url) {
+        driver.get(url);
+    }
+
+    public void waitForPageToLoad() {
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(webDriver -> ((org.openqa.selenium.JavascriptExecutor) webDriver)
+                        .executeScript("return document.readyState").equals("complete"));
+    }
+
+    public void waitForElementToBeClickable(By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+
 }
